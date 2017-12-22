70a574cbd17d05f2240fbd0a0069210336a166f0
lujianmei <anysky130@163.com>
2017-12-22 14:17:20 +0800

    changed

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-for i in {1..$(cat Vagrantfile| awk '/^\$num_instances/{print $3}')} ; do vagrant up core0${i} ; done
+for i in {1..$(cat Vagrantfile| awk '/^\$num_instances/{print $3}')} ; do vagrant up centos0${i} ; done
